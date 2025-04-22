b60987ebdf8e4749780784c0612bdc773e38e3b1
MpourmoghaddamZand <mortezapourmoghaddam99@gmail.com>
2025-04-22 16:22:13 +0330

    add Number jsx

--- public/svg/index.js
+++ public/svg/index.js
@@ -2,11 +2,15 @@ import shopSVG from './shopping-cart.svg'
 import menuSVG from './menu.svg'
 import searchSVG from './search.svg'
 import tomanSVG from './toman.svg'
+import plussSVG from './plus.svg'
+import minusSVG from './minus.svg'
 
 export { 
     menuSVG, 
     shopSVG,
     searchSVG,
     tomanSVG,
+    plussSVG,
+    minusSVG,
 }
 
--- src/components/util/Button.jsx
+++ src/components/util/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({text}) => {
+const Button = ({ text, onClick }) => {
   return (
-    <div className='flex justify-center items-center py-2 w-full bg-primary rounded-full'>
-        <p className='font-Pinar-bold text-[12px] text-white text-right [direction:rtl]'> {text} </p>
+    <div onClick={onClick} className='flex justify-center items-center py-2 w-full bg-primary rounded-full'>
+      <p className='font-Pinar-bold text-[12px] text-white text-right [direction:rtl]'> {text} </p>
     </div>
   )
 }
