5df3d96c1fa106dd0d8f76e769c2bc0cceb0a000
MpourmoghaddamZand <mortezapourmoghaddam99@gmail.com>
2025-04-28 09:16:40 +0330

    cart context

--- src/Context.jsx
+++ src/Context.jsx
@@ -2,8 +2,9 @@ import React, { createContext, useState } from 'react';
 export const SharedContext = createContext();
 export const SharedProvider = ({ children }) => {
   const [popUpIsOpen, setPopUpIsOpen] = useState(false);
+  const [cart, setCart] = useState({});
   return (
-    <SharedContext.Provider value={{ popUpIsOpen, setPopUpIsOpen }}>
+    <SharedContext.Provider value={{ popUpIsOpen, setPopUpIsOpen, cart, setCart }}>
       {children}
     </SharedContext.Provider>
   );
