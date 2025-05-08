27fab02f896c8d37a3e51189513a6858c36fc0f9
MpourmoghaddamZand <mortezapourmoghaddam99@gmail.com>
2025-05-08 12:58:49 +0330

    update Category and navbar components and el

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { BurgerSVG } from './svg/SvgCategory'
 
 const Categorylist = [
@@ -27,20 +27,38 @@ const CategoryItem = ({ title, children, active, onClick }) => {
 
 const Category = () => {
     const [activeId, setActiveId] = useState(null)
-
+    const [isSticky, setIsSticky] = useState(false);
+    const sentinelRef = useRef(null);
+    useEffect(() => {
+        const el = sentinelRef.current;
+        if (!el) return;
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsSticky(!entry.isIntersecting);
+            },
+            { threshold: 1 }
+        );
+        observer.observe(el);
+        return () => observer.disconnect();
+    }, []);
     return (
-        <div className='flex gap-5 my-20 mx-5'>
-            {Categorylist.map((item) => (
-                <CategoryItem
-                    key={item.id}
-                    title={item.title}
-                    active={activeId === item.id}
-                    onClick={() => setActiveId(item.id)}
-                >
-                    <BurgerSVG />
-                </CategoryItem>
-            ))}
-        </div>
+        <>
+            <div ref={sentinelRef} style={{ height: 1 }} />
+            <div className={`w-full bg-bg-color z-10 sticky top-[88px] drop-shadow-md ${isSticky ? "drop-shadow-lg" : "drop-shadow-none"}`}>
+                <div className={`flex  gap-5 mt-7 mb-14 mx-5 pb-5`} >
+                    {Categorylist.map((item) => (
+                        <CategoryItem
+                            key={item.id}
+                            title={item.title}
+                            active={activeId === item.id}
+                            onClick={() => setActiveId(item.id)}
+                        >
+                            <BurgerSVG />
+                        </CategoryItem>
+                    ))}
+                </div>
+            </div>
+        </>
     )
 }
 
